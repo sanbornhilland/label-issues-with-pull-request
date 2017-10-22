6874b19a97f20db36703e6d43e21475c9c148898
sanborn <amadeus.hilland@gmail.com>
2017-10-22 00:02:54 -0400

    Remove labels when PR closed. Create label if it doesn't exist

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
 const parser = require('./parser.js')
 
+const LABEL = 'pull request open'
+const PINK = 'f767eb'
+
+async function getClosedIssues(context) {
+  const issueInfo = context.issue()
+  const commits = await context.github.pullRequests.getCommits(issueInfo)
+
+  const filteredMessages = commits.data
+    .map(({ commit }) => commit.message.toLowerCase())
+    .join('\n')
+
+  return parser.extractIssueNumbers(filteredMessages)
+}
+
+async function createLabelIfNecessary(context) {
+  const repoInfo = context.repo()
+  const labels = await context.github.issues.getLabels(repoInfo)
+
+  if (!labels.data.some(({ name }) => name === LABEL)) {
+    await context.github.issues.createLabel({
+      ...repoInfo,
+      name: LABEL,
+      color: PINK,
+    })
+  }
+}
+
 module.exports = (robot) => {
-  robot.on('issues.opened', async context => {
-    robot.log('Issue Opened event received')
-    robot.log(context.payload)
-    robot.log(context.issue.title)
+  robot.on('pull_request.closed', async context => {
+    const closedIssues = await getClosedIssues(context)
+
+    closedIssues.forEach((issue) => {
+      context.github.issues.removeLabel(context.repo({
+        number: issue,
+        name: LABEL,
+      }))
+    })
   })
 
   robot.on('pull_request.opened', async context => {
-    const { repository, number } = context.payload
-    const { data } = await context.github.pullRequests.getCommits({
-      owner: repository.owner.login,
-      repo: repository.name,
-      number,
-    })
-
-    const filteredMessages = data
-      .map(({ commit }) => commit.message.toLowerCase())
-      .join('\n')
-
-    const closedIssues = parser.extractIssueNumbers(filteredMessages)
-
-    robot.log('Closed issues ->', closedIssues)
+    const repoInfo = context.repo()
+    const closedIssues = await getClosedIssues(context)
+    await createLabelIfNecessary(context)
 
     closedIssues.forEach((issue) => {
       context.github.issues.addLabels({
-        owner: repository.owner.login,
-        repo: repository.name,
+        ...repoInfo,
         number: issue,
-        labels: ['pull request open'],
+        labels: [LABEL],
       })
     })
   })
