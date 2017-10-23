4d35a5c2f0af2fde4b12c6023631ce14ffd62301
sanborn <amadeus.hilland@gmail.com>
2017-10-23 19:54:38 -0400

    Add better logging

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ async function getClosedIssues(context, keywords) {
   return parser.extractIssueNumbers(filteredMessages, keywords)
 }
 
-async function createLabelIfNecessary(context, labelName, labelColor) {
+async function createLabelIfNecessary(context, robot, labelName, labelColor) {
   const repoInfo = context.repo()
   const labels = await context.github.issues.getLabels(repoInfo)
 
   if (!labels.data.some(({ name }) => name === labelName)) {
+    robot.debug(`No label with name ${name} found. Creating a new label...`)
+
     await context.github.issues.createLabel({
       ...repoInfo,
       name: labelName,
@@ -25,27 +27,36 @@ async function createLabelIfNecessary(context, labelName, labelColor) {
   }
 }
 
-async function getUserConfig(context) {
+async function getUserConfig(context, robot) {
   const userConfig = await context.config('config.yml')
+
+  robot.debug('User defined config values: ', userConfig)
 
   return userConfig && userConfig.labelPullRequests ? userConfig.labelPullRequests : {}
 }
 
-async function getConfig(context) {
+async function getConfig(context, robot) {
   const userConfig = await getUserConfig(context)
 
-  return {
+  const config = {
     ...defaultConfig,
     ...userConfig,
   }
+
+  robot.debug('Using config: ', config)
+
+  return config
 }
 
 module.exports = (robot) => {
   robot.on('pull_request.closed', async context => {
-    const { labelName, keywords } = await getConfig(context)
+    const { labelName, keywords } = await getConfig(context, robot)
     const closedIssues = await getClosedIssues(context, keywords)
 
+    robot.debug('[PR CLOSED] Issues closed by this PR: ', closedIssues)
+
     closedIssues.forEach((issue) => {
+      robot.debug(`[PR CLOSED] Removing label ${labelName} from ${issue}`)
       // I don't know why I can't do await with a try/catch here.
       context.github.issues.removeLabel(context.repo({
         number: issue,
@@ -56,12 +67,16 @@ module.exports = (robot) => {
   })
 
   robot.on('pull_request.opened', async context => {
-    const { labelName, labelColor, keywords } = await getConfig(context)
+    const { labelName, labelColor, keywords } = await getConfig(context, robot)
     const closedIssues = await getClosedIssues(context, keywords)
-    await createLabelIfNecessary(context, labelName, labelColor)
+    await createLabelIfNecessary(context, robot, labelName, labelColor)
     const repoInfo = context.repo()
 
+    robot.debug('[PR OPENED] Issues that will be closed by this PR: ', closedIssues)
+
     closedIssues.forEach((issue) => {
+      robot.debug(`[PR OPENED] Adding label ${labelName} to issue ${issue}`)
+
       context.github.issues.addLabels({
         ...repoInfo,
         number: issue,
