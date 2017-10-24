3a28010e7553711803ce5c529672af88909235be
sanborn <amadeus.hilland@gmail.com>
2017-10-23 22:42:13 -0400

    Correct log statements...because I actually read the docs

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ async function createLabelIfNecessary(context, robot, labelName, labelColor) {
   const labels = await context.github.issues.getLabels(repoInfo)
 
   if (!labels.data.some(({ name }) => name === labelName)) {
-    robot.debug(`No label with name ${name} found. Creating a new label...`)
+    robot.log.debug(`No label with name ${name} found. Creating a new label...`)
 
     await context.github.issues.createLabel({
       ...repoInfo,
@@ -30,7 +30,7 @@ async function createLabelIfNecessary(context, robot, labelName, labelColor) {
 async function getUserConfig(context, robot) {
   const userConfig = await context.config('config.yml')
 
-  robot.debug('User defined config values: ', userConfig)
+  robot.log.debug('User defined config values: ', userConfig)
 
   return userConfig && userConfig.labelPullRequests ? userConfig.labelPullRequests : {}
 }
@@ -43,7 +43,7 @@ async function getConfig(context, robot) {
     ...userConfig,
   }
 
-  robot.debug('Using config: ', config)
+  robot.log.debug('Using config: ', config)
 
   return config
 }
@@ -55,10 +55,10 @@ module.exports = (robot) => {
     await createLabelIfNecessary(context, robot, labelName, labelColor)
     const repoInfo = context.repo()
 
-    robot.debug('[PR OPENED] Issues that will be closed by this PR: ', closedIssues)
+    robot.log.debug('[PR OPENED] Issues that will be closed by this PR: ', closedIssues)
 
     closedIssues.forEach((issue) => {
-      robot.debug(`[PR OPENED] Adding label ${labelName} to issue ${issue}`)
+      robot.log.debug(`[PR OPENED] Adding label ${labelName} to issue ${issue}`)
 
       context.github.issues.addLabels({
         ...repoInfo,
@@ -72,10 +72,10 @@ module.exports = (robot) => {
     const { labelName, keywords } = await getConfig(context, robot)
     const closedIssues = await getClosedIssues(context, keywords)
 
-    robot.debug('[PR CLOSED] Issues closed by this PR: ', closedIssues)
+    robot.log.debug('[PR CLOSED] Issues closed by this PR: ', closedIssues)
 
     closedIssues.forEach((issue) => {
-      robot.debug(`[PR CLOSED] Removing label ${labelName} from ${issue}`)
+      robot.log.debug(`[PR CLOSED] Removing label ${labelName} from ${issue}`)
       // I don't know why I can't do await with a try/catch here.
       context.github.issues.removeLabel(context.repo({
         number: issue,
